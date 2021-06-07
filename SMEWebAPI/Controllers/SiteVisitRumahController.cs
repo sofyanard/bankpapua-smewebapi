f76b3dc5b3546886cd85fc0d449e2ac712982e6f
Sofyan Ardianto <sofyanard@gmail.com>
2021-06-07 14:35:00 +0700

    Site Visit Usaha, Rumah, Kantor - mengubah query get list pipeline menjadi berdasarkan assignment

--- SMEWebAPI/Controllers/SiteVisitRumahController.cs
+++ SMEWebAPI/Controllers/SiteVisitRumahController.cs
@@ -31,6 +31,8 @@ namespace SMEWebAPI.Controllers
             try
             {
                 string email = User.Identity.GetUserName();
+
+                /*
                 conn.QueryString = "SELECT USERID FROM SCUSER WHERE SU_EMAIL = '" + email + "'";
                 conn.ExecuteQuery();
 
@@ -40,6 +42,7 @@ namespace SMEWebAPI.Controllers
                 {
                     throw new Exception("User email is not found");
                 }
+                */
 
                 conn.QueryString = "SELECT IN_SMALL, IN_MIDDLE, IN_CORPORATE, IN_MICRO FROM RFINITIAL";
                 conn.ExecuteQuery();
@@ -49,6 +52,7 @@ namespace SMEWebAPI.Controllers
                 string mInCorp = conn.GetFieldValue("IN_CORPORATE");
                 string mInCons = conn.GetFieldValue("IN_MICRO");
 
+                /*
                 conn.QueryString = "select distinct a.AP_REGNO, a.CU_REF, a.AP_RELMNGR as CU_RM, " +
                     "case c.CU_CUSTTYPEID when '01' then isnull(c3.COMPTYPEDESC, '') +' ' + isnull(c2.CU_COMPNAME, '') " +
                     "else isnull(c1.CU_FIRSTNAME, '') + ' ' + isnull(c1.CU_MIDDLENAME, '') + ' ' + isnull(c1.CU_LASTNAME, '') end as CU_NAME, " +
@@ -63,6 +67,23 @@ namespace SMEWebAPI.Controllers
                     "and t.AP_CURRTRACK = 'BP5.0' " + // *** temporary hard-coded
                     "and a.AP_COMPLEVEL in ('" + mInCons + "') " + // *** temporary hard-coded
                     "and a.AP_RELMNGR = '" + userid + "'";
+                */
+
+                conn.QueryString = "select distinct a.AP_REGNO, a.CU_REF, a.AP_RELMNGR as CU_RM, " +
+                    "case c.CU_CUSTTYPEID when '01' then isnull(c3.COMPTYPEDESC, '') +' ' + isnull(c2.CU_COMPNAME, '') " +
+                    "else isnull(c1.CU_FIRSTNAME, '') + ' ' + isnull(c1.CU_MIDDLENAME, '') + ' ' + isnull(c1.CU_LASTNAME, '') end as CU_NAME, " +
+                    "a.AP_SITEVISITSTA " +
+                    "from APPLICATION a " +
+                    "join CUSTOMER c on a.CU_REF = c.CU_REF " +
+                    "left join CUST_PERSONAL c1 on c.CU_REF = c1.CU_REF " +
+                    "left join CUST_COMPANY c2 on c.CU_REF = c2.CU_REF " +
+                    "left join RFCOMPTYPE c3 on c2.CU_COMPTYPE = c3.COMPTYPEID " +
+                    "join APPTRACK t on a.AP_REGNO = t.AP_REGNO " +
+                    "join SITEVISITASSIGNMENT v on a.AP_REGNO = v.AP_REGNO " +
+                    "where a.AP_REJECT <> '1' and a.AP_CANCEL <> '1' " +
+                    "and t.AP_CURRTRACK = 'BP5.0' " + // *** temporary hard-coded
+                    "and a.AP_COMPLEVEL in ('" + mInCons + "') " + // *** temporary hard-coded
+                    "and v.OFFICERID = '" + email + "'";
                 conn.ExecuteQuery();
 
                 DataTable dtResult = conn.GetDataTable().Copy();
